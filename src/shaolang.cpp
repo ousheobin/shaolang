5b1bba3802d1a518461b145ef171782641ffc707
Shaobin.Ou <me@oushaobin.cn>
2018-12-20 23:48:33 +0800

    Try something ~

--- src/shaolang.cpp
+++ src/shaolang.cpp
@@ -1,8 +1,24 @@
-#include<iostream>
+//
+//  shaolang.cpp
+//  shaolang
+//
+//  Created by Ou Sheobin on 2018/12/20.
+//
+
+#include <iostream>
+#include "shaolang.hpp"
 
 using namespace std;
 
-int main(void){
-    cout<<"Hello Shaolang!!!"<<endl;
+int main(int argc, char *argv[]){
+    if( argc < 2){
+        cout << "Error: Nothing is specified. \nUsage: \nshaolang xxx.sheo"<<endl;
+    }
+    base_path = new string(argv[0]);
+    string * target_file = new string(argv[1]);
+#ifdef DEBUG
+    cout <<"base path:"<< *base_path << endl;
+    cout <<"target file:"<< *target_file << endl;
+#endif
     return 0;
 }
---
+++ src/shaolang.hpp
@@ -0,0 +1,19 @@
+//
+//  shaolang.hpp
+//  shaolang
+//
+//  Created by Ou Sheobin on 2018/12/20.
+//
+
+#include<string>
+
+using namespace std;
+
+#ifndef shaolang_hpp
+#define shaolang_hpp
+
+#define DEBUG 1
+
+string * base_path;
+
+#endif /* shaolang_hpp */
